b69ae52d70fe50d0d5b5e99af8f5ad8d49cbd51e
HelenQuang <qvha.97@gmail.com>
2022-02-04 18:09:47 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Github Finder App (Learning Project)
+
+A React app to search GitHub profiles and see profile details by using Github API.
+
+DEMO link:
---
+++ src/components/users/UserItem.jsx
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+
+function UserItem({ user }) {
+  return <div>{user.login}</div>;
+}
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+
+export default UserItem;

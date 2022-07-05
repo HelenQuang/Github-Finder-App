2a04edae358334ff598dd9ba282c3b58d4110e1a
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-07-05 20:23:55 +0300

    Fix Github Token

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,17 +3,7 @@ function About() {
     <div>
       <div className="h1 text-6xl mb-4">Github Finder</div>
       <p className="mb-4 text-2xl font-light">
-        A React app to search GitHub profiles and see profile details. This
-        project is part of the
-        <a href="https://www.udemy.com/course/react-front-to-back-2022/">
-          {" "}
-          React Front To Back
-        </a>{" "}
-        Udemy course by
-        <strong>
-          <a href="https://traversymedia.com"> Brad Traversy</a>
-        </strong>
-        .
+        A React app to search GitHub profiles and see profile details.
       </p>
       <p className="text-lg text-gray-400">
         Version <span className="text-gray">1.0.0</span>

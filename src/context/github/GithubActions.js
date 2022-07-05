d4fd8e1e8b9580ab5d7e13ff567c4ec49c910e35
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-07-05 20:35:01 +0300

    Fix Github Token

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN_NEW;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 //Using axios instead of fetch
 const github = axios.create({
